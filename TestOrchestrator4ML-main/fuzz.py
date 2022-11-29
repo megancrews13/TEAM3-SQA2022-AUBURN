4d0ffddc6e35d386688b683be53dc0e555943428
megancrews13 <megancrews13@yahoo.com>
2022-11-29 17:29:53 -0600

    fuzz.py

--- TestOrchestrator4ML-main/fuzz.py
+++ TestOrchestrator4ML-main/fuzz.py
@@ -1,6 +1,8 @@
 import generation.constants as constants
 from generation.py_parser import checkAlgoNames
 from label_perturbation_attack.knn import euc_dist
+from label_perturbation_attack.knn import calculate_metrics
+from label_perturbation_attack.main import call_loss
 
 def fuzzingCheckAlgoNames():
         #inputs to try
@@ -32,9 +34,39 @@ def fuzzingEuc_Dist():
                            print ("euc_dist failed with " + str(val1) + " and " + str(val2))
            print ("Finished fuzzing.")
 
+def fuzzingCalculate_Metrics():
+           #inputs to try
+           fuzzingValues = [x, "toy", 24]
+           print ("Start the fuzzing.")
+
+           #looping to search for errors
+           for inputs in fuzzingValues:
+               try:
+                   calculate_metrics(input)
+                   print ("calculate_metrics passed with " + str(input))
+               except:
+                   print ("calculate_metrics failed with " + str(input))
+           print ("Finished fuzzing.")
+
+def fuzzingCall_Loss():
+           #inputs to try
+           fuzzingValues = [("good-model-name"), (0), ({}), (None)]
+           print ("Start the fuzzing.")
+
+           #looping to search for errors
+           for inputs in fuzzingValues:
+               try:
+                   call_loss(input)
+                   print ("call_loss passed with " + str(input))
+               except:
+                   print ("call_loss failed with " + str(input))
+           print ("Finished fuzzing.")
+
 def main():
     fuzzingCheckAlgoNames()
     fuzzingEuc_Dist()
+    fuzzingCalculate_Metrics()
+    fuzzingCall_Loss()
 
 if __name__ == "__main__":
     main()
