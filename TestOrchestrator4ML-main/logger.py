cb608891916355d476e61b0d59e39163767d5567
Will Thompson <33556333+wsthomp00@users.noreply.github.com>
2022-12-01 15:58:34 -0600

    Add files via upload

--- TestOrchestrator4ML-main/logger.py
+++ TestOrchestrator4ML-main/logger.py
@@ -1,6 +1,6 @@
 import logging
 
 def getLoggerObj():
-    logging.basicConfig(filename="log.LOG", filemode='w', format='%(asctime)s:::%(name)s:::%(levelname)s:::%(message)s', datefmt='%d-%b-%y %H:%M:%S')
-    loggerObj = logging.getLogger('gen-logger')
+    logging.basicConfig(filename="log.LOG", level=logging.INFO, format='%(asctime)s:::%(name)s:::%(levelname)s:::%(message)s', datefmt='%d-%b-%y %H:%M:%S')
+    loggerObj = logging.getLogger('logger')
     return loggerObj
